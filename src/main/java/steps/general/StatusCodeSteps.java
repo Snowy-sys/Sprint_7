e113c90f3c76ceae6bd1dc6c381c2fad104f10f0
Nikulin Maksim <maxveter2000@gmail.com>
2025-11-20 15:30:16 +0300

    Поправил статус коды в StatusCodeSteps, взяв переменные из библиотеки

--- src/main/java/steps/general/StatusCodeSteps.java
+++ src/main/java/steps/general/StatusCodeSteps.java
@@ -3,30 +3,32 @@ package steps.general;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
+import static org.apache.http.HttpStatus.*;
+
 public class StatusCodeSteps {
     @Step("Проверка негативного статус кода 409")
     public void checkNegativeStatusCode409(Response response) {
-        response.then().statusCode(409);
+        response.then().statusCode(SC_CONFLICT);
     }
 
     @Step("Проверка негативного статус кода 400")
     public void checkNegativeStatusCodeWithoutRequiredFields400(Response response){
-        response.then().statusCode(400);
+        response.then().statusCode(SC_BAD_REQUEST);
     }
 
     @Step("Проверка статуса кода 201")
     public void checkSuccessfulCodeStatus201(Response response){
-        response.then().statusCode(201);
+        response.then().statusCode(SC_CREATED);
     }
 
     @Step("Проверка статуса кода 200")
     public void checkSuccessfulCodeStatus200(Response response){
-        response.then().statusCode(200);
+        response.then().statusCode(SC_OK);
     }
 
     @Step("Проверка статуса кода 404")
     public void checkCodeStatusWithErrorInFields404(Response response){
-        response.then().statusCode(404);
+        response.then().statusCode(SC_NOT_FOUND);
     }
 
 }
